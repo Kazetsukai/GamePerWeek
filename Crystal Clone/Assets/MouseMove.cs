b2d1a5fb1fc7531d5ae60da45390f77f6cfeeacd
Daniel Tebbutt <moeruze.burning+github@gmail.com>
2015-11-27 07:43:07 +1300

    Added an iceberg with a kitten on it

---
+++ Crystal Clone/Assets/MouseMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseMove : MonoBehaviour {
+
+    Vector3 _oldMousePos;
+    Vector3 _velocity;
+    bool _firstFrame = true;
+
+    Quaternion _oldRotation;
+
+    CharacterController _controller;
+
+	// Use this for initialization
+	void Start () {
+        _oldRotation = transform.localRotation;
+        _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mousePos = Input.mousePosition;
+
+        var delta = mousePos - _oldMousePos;
+        delta /= 2000;
+        delta.z = delta.y;
+        delta.y = 0;
+
+        _velocity += delta;
+        
+        _controller.Move(_velocity);
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.LookRotation(_velocity.normalized, Vector3.up), 10);
+
+        _oldMousePos = mousePos;
+	}
+}
