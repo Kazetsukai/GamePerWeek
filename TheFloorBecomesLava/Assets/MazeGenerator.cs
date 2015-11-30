f0c32b4e73dbd7294de8e538be934cdcd79223ea
Daniel Tebbutt <moeruze.burning+github@gmail.com>
2015-12-01 07:16:04 +1300

    Blocks raising etc

--- TheFloorBecomesLava/Assets/MazeGenerator.cs
+++ TheFloorBecomesLava/Assets/MazeGenerator.cs
@@ -1,31 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MazeGenerator : MonoBehaviour {
+public class MazeGenerator : MonoBehaviour
+{
+
+    public const float BlockScale = 0.3f;
 
     public GameObject ProtoWall;
 
     public GameObject[,] _wallGrid;
 
+    private int _height;
+    private int _width;
+
     // Use this for initialization
-    void Start() {
-        
-        var width = (int)(10 * transform.localScale.x);
-        var height = (int)(10 * transform.localScale.z);
+    void Start()
+    {
+        InitialiseGrid();
+    }
 
-        _wallGrid = new GameObject[width, height];
+    private void InitialiseGrid()
+    {
+        _width = (int)(10 * transform.localScale.x / BlockScale);
+        _height = (int)(10 * transform.localScale.z / BlockScale);
 
-        for (int x = 0; x < width; x++)
+        _wallGrid = new GameObject[_width, _height];
+
+        for (int x = 0; x < _width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < _height; y++)
             {
-                _wallGrid[x, y] = (GameObject)Instantiate(ProtoWall, new Vector3(x - width / 2 + 0.5f, 0, y - height / 2 + 0.5f), Quaternion.identity);
+                _wallGrid[x, y] = (GameObject)Instantiate(ProtoWall, new Vector3(x - _width / 2, 0, y - _height / 2) * BlockScale, Quaternion.identity);
+                _wallGrid[x, y].transform.localScale = new Vector3(BlockScale, BlockScale, BlockScale);
+
+                ///////////////////////
+                if (Random.value < 0.4f)
+                {
+                    _wallGrid[x, y].SendMessage("Rise");
+                }
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ TheFloorBecomesLava/Assets/WallBlock.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBlock : MonoBehaviour
+{
+    private bool _up;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var target = (_up ? 0.5f : -0.55f) * transform.localScale.y;
+
+        if (transform.localPosition.y < 0.5)
+        {
+            transform.localPosition = new Vector3(
+                transform.localPosition.x,
+                transform.localPosition.y + Mathf.Clamp(target - transform.localPosition.y, -Time.fixedDeltaTime, Time.fixedDeltaTime),
+                transform.localPosition.z
+            );
+        }
+    }
+
+    void Drop()
+    {
+        _up = false;
+    }
+
+    void Rise()
+    {
+        _up = true;
+    }
+}
