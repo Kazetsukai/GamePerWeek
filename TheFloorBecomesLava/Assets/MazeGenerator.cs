ed1a8373b472df48c2c60d8e857c66628ee3edf4
Daniel Tebbutt <moeruze.burning+github@gmail.com>
2015-12-01 06:39:39 +1300

    Start of TheFloorBecomesLava, made a grid

---
+++ TheFloorBecomesLava/Assets/MazeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeGenerator : MonoBehaviour {
+
+    public GameObject ProtoWall;
+
+    public GameObject[,] _wallGrid;
+
+    // Use this for initialization
+    void Start() {
+        
+        var width = (int)(10 * transform.localScale.x);
+        var height = (int)(10 * transform.localScale.z);
+
+        _wallGrid = new GameObject[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                _wallGrid[x, y] = (GameObject)Instantiate(ProtoWall, new Vector3(x - width / 2 + 0.5f, 0, y - height / 2 + 0.5f), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
