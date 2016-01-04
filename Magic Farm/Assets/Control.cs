d054722108db9f5e58128a7bf6a8fad7f9d56831
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-01-04 18:57:58 +1300

    Bleh

--- Magic Farm/Assets/Control.cs
+++ Magic Farm/Assets/Control.cs
@@ -2,14 +2,36 @@
 using System.Collections;
 
 public class Control : MonoBehaviour {
+    private CharacterController _controller;
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject PlantProto;
+
+    // Use this for initialization
+    void Start () {
+        _controller = GetComponent<CharacterController>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    void Update()
+    {
+        if (_controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        _controller.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(PlantProto, transform.localPosition, Quaternion.identity);
+        }
+    }
 }
---
+++ Magic Farm/Assets/Follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject Target;
+
+    private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+        _offset = Target.transform.localPosition - transform.localPosition;
+	}
+	
+	void LateUpdate () {
+        transform.localPosition = Target.transform.localPosition - _offset;
+	}
+}
