5ef69e2b5b119fd03d1a671a0202d3105d9b21a4
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-02-28 19:44:42 +1300

    Battlecats

---
+++ BattleCats/Assets/GameBoard.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBoard : MonoBehaviour {
+
+    public int BoardHeight;
+    public int BoardWidth;
+
+    public GameObject GrassTileProto;
+
+    GameTile[,] Tiles;
+    private GameTile _highlightedTile;
+    private GameTile _selectedTile;
+
+    // Use this for initialization
+    void Start () {
+        Tiles = new GameTile[BoardHeight, BoardWidth];
+
+        // Make a board of grass tiles.
+        for (int y = 0; y < BoardHeight; y++)
+        {
+            for (int x = 0; x < BoardWidth; x++)
+            {
+                var tile = Instantiate(GrassTileProto);
+                tile.transform.position = new Vector3(x - BoardWidth / 2.0f, y - BoardHeight / 2.0f, 0);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+        
+        // Raycast from mouse to highlight and select tiles
+        if (Physics.Raycast(ray, out rayHit))
+        {
+            var hitObj = rayHit.collider.gameObject;
+
+            var gameTile = hitObj.GetComponent<GameTile>();
+            if (gameTile != null)
+            {
+                if (_highlightedTile != gameTile)
+                {
+                    if (_highlightedTile != null)
+                    {
+                        _highlightedTile.SetHighlighted(false);
+                    }
+                    _highlightedTile = gameTile;
+                    _highlightedTile.SetHighlighted(true);
+                }
+            }
+
+            // Select when we click on something
+            if (gameTile != _selectedTile)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (_selectedTile != null)
+                        _selectedTile.SetSelected(false);
+
+                    _selectedTile = gameTile;
+
+                    if (_selectedTile != null)
+                        _selectedTile.SetSelected(true);
+                }
+            }
+        }
+    }
+
+    public GameTile GetSelectedTile()
+    {
+        return _selectedTile;
+    }
+}
---
+++ BattleCats/Assets/GameTile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameTile : MonoBehaviour {
+    private bool _highlighted;
+    private bool _selected;
+
+    private Color _origColor;
+
+    private Renderer _renderer;
+
+    // Use this for initialization
+    void Start () {
+        _renderer = GetComponent<Renderer>();
+        _origColor = _renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateColor();
+    }
+
+    public void SetHighlighted(bool highlighted)
+    {
+        _highlighted = highlighted;
+    }
+
+    public void SetSelected(bool selected)
+    {
+        _selected = selected;
+    }
+
+    private void UpdateColor()
+    {
+        if (_selected)
+            _renderer.material.color = Color.Lerp(_origColor, Color.black, 0.3f - (Mathf.Sin(Time.realtimeSinceStartup * 6) / 5.0f));
+        else if (_highlighted)
+            _renderer.material.color = Color.Lerp(_origColor, Color.yellow, 0.5f);
+        else
+            _renderer.material.color = _origColor;
+    }
+
+}
+
+public enum SelectState
+{
+    Unselected,
+    Highlighted,
+    Selected
+}
